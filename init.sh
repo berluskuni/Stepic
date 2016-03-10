c75cafa7216e7da2fee4b0bbb4f2f4bb3679dc73
berluskuni <berluskuni@gmail.com>
2016-03-10 15:23:50 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-sudo ﻿ln -sf /home/berluskuni/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/berluskuni/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/berluskuni/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+
