b2fd6e61a7026405b2f402e8c60ff5058c5f0187
berluskuni <berluskuni@gmail.com>
2016-03-11 12:34:50 +0200

    2

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     qs = environ['QUERY_STRING'].split("&")
-    resp = [item+"\r\n"] for item in qs]
+    resp = [item+"\r\n" for item in qs]
     return resp
