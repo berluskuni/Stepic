fe33775e08886430c167dd7e4cb9147bcdb2aad1
berluskuni <berluskuni@gmail.com>
2016-03-11 12:39:48 +0200

    2

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
+from  cgi import parse_qs
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    qs = environ['QUERY_STRING'].split("&")
-    resp = [item+"\r\n" for item in qs]
-    return resp
+    qs = parse_qs(environ['QUERY_STRING'])
+    return ['%s=%s<br>' % (x, qs[x][0]) for x in qs]
