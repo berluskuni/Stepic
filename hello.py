5e85c45f7e7aa9d3f70c91a3881242005e18e49a
berluskuni <berluskuni@gmail.com>
2016-03-11 12:17:56 +0200

    2

--- hello.py
+++ hello.py
@@ -2,4 +2,4 @@ from  cgi import parse_qs
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     qs = parse_qs(environ['QUERY_STRING'])
-    return ['%s=%s' % (k, qs[k][0]) for k in qs]
+    return ['%s=%s' % (x, qs[x][0]) for x in qs]
