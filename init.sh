cbb6aeb07edc175112bfcbf49a7a38137b75cfdb
berluskuni <berluskuni@gmail.com>
2016-03-11 11:11:21 +0200

    2

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /rtc/init.d/gunicorv restart
+sudo /etc/init.d/gunicorv restart
